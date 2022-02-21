1bc4b087df21e18f4b9f45907194e161fd7d7e37
andrea <andrea.tatti.2003@calvino.edu.it>
2022-02-21 22:07:10 +0100

    Sono andato avanti con le recensioni

---
+++ pagine/user/recensioni/insertRecensione.php
@@ -0,0 +1,17 @@
+<?php
+    session_start();
+    $host="127.0.0.1";
+    $user="root";
+    $password="";
+    $database="getnbite";
+
+    $connessione = new mysqli($host, $user, $password, $database);
+
+    if($connessione === false){
+        die("Errore di connessione: ".$connessione->connect_error);
+    }
+
+    
+
+    sql ="INSERT INTO recensione";
+?>
