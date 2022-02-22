0302ac6095ab7f089724c922bdbdf4120e241cbb
andrea tatti <andrea.tatti.2003@calvino.edu.it>
2022-02-22 14:11:19 +0100

    Inizio registrazione e prosecuzione login

---
+++ pagine/user/registra/registra/registra.php
@@ -0,0 +1,14 @@
+<?php
+	session_start();
+
+    $host="127.0.0.1";
+    $user="root";
+    $password="";
+    $database="getnbite";
+
+    $connessione= new mysqli($host, $user, $password , $database);
+
+    if($connessione === false){
+        die
+    }
+?>
