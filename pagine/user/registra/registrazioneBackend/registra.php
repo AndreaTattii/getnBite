cd3cd6a5b73defb4b0c5c7c171ea56a6e5f46207
andrea <andrea.tatti.2003@calvino.edu.it>
2022-02-22 19:59:55 +0100

    Il form funziona anche con database, ancora da affinare.

--- pagine/user/registra/registrazioneBackend/registra.php
+++ pagine/user/registra/registrazioneBackend/registra.php
@@ -9,6 +9,22 @@
     $connessione= new mysqli($host, $user, $password , $database);
 
     if($connessione === false){
-        die
+        die("Errore di connessione: ".$connessione->connect_error);
+    }
+
+    $nome = $connessione->real_escape_string($_REQUEST['nome']);
+    $cognome = $connessione->real_escape_string($_REQUEST['cognome']);
+    $mail = $connessione->real_escape_string($_REQUEST['mail']);
+    $password = $connessione->real_escape_string($_REQUEST['password']);
+    
+
+
+    $sql="INSERT INTO utente (nome, cognome, email, pass) VALUES 
+    ('$nome','$cognome', '$mail', '$password')";
+
+    if($connessione->query($sql) === true){
+        echo "Utente inserito con successo";
+    }else{
+        echo "Errore durante inserimento: ".$connessione->error;
     }
 ?>
