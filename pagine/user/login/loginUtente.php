47e97e1cadb11fb34ea3e3f6e9aabe7c6a55f94f
Giovanni-Pascoli <daniele.scaffai.2003@calvino.edu.it>
2022-02-24 11:47:38 +0100

    Ho tolto dei commenti

--- pagine/user/login/loginUtente.php
+++ pagine/user/login/loginUtente.php
@@ -8,33 +8,6 @@
         $database="getnbite";
     
         $connessione= new mysqli($host, $user, $pass , $database);
-       
-    
-        /* if($connessione === false){
-            die("Errore di connessione: ".$connessione->connect_error);
-        }
-    
-        $mail = $connessione->real_escape_string($_REQUEST['mail']);
-        $password = $connessione->real_escape_string($_REQUEST['password']);
-        
-        
-        if (isset($_SESSION['mail'])) {
-            header("Location: ../../../index.php");
-        }     
-        if (isset($_POST['submit'])) {
-            $mail = $_POST['mail'];
-            $password = hash($_POST['password']);  //hashing della password
-        
-            $sql = "SELECT * FROM users WHERE email='$mail' AND pass='$password'";
-            $result = mysqli_query($connessione, $sql);
-            if ($result->num_rows > 0) {
-                $row = mysqli_fetch_assoc($result);
-                $_SESSION['mail'] = $row['mail'];
-                header("Location: ../../../index.php");
-            } else {
-                echo "<script>alert('Woops! Email or Password is Wrong.')</script>";
-            }
-        } */
         
         error_reporting(0);
 
