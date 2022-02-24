2155594c5590e812fb03b0390dea34765e9c98ee
andrea <andrea.tatti.2003@calvino.edu.it>
2022-02-24 18:29:07 +0100

    Fatte correzioni

--- pagine/user/contatti/contatti.php
+++ pagine/user/contatti/contatti.php
@@ -50,7 +50,7 @@
 			<!-- NAVABAR PER UTENTE NON LOGGATO -->
 			<nav class="navbar  navbar-expand-lg navbar-light bg-light">
 					<div class="container p-2">
-						<a class="navbar-brand" href="./">
+						<a class="navbar-brand" href="../../../../index.php">
 							<img src="../../../img/Home/logoScritta.png" alt="" height="50" class="d-inline-block align-text-top">
 						</a>
 						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation" >
