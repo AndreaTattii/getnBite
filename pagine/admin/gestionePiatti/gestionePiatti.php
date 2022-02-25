f935400a6ad223cafd1e46009c65e64df9b24e35
Giovanni <daniele.scaffai.2003@calvino.edu.it>
2022-02-25 09:22:22 +0100

    Prova gestione piatti

--- pagine/admin/gestionePiatti/gestionePiatti.php
+++ pagine/admin/gestionePiatti/gestionePiatti.php
@@ -1,9 +1,16 @@
 <?php  
- $connect = mysqli_connect("localhost", "root", "", "testing");  
+
+$host="127.0.0.1";
+$user="root";
+$pass="";
+$database="getnbite";
+
+ $connect = mysqli_connect($host, $user, $pass, $database);  
+
  if(isset($_POST["insert"]))  
  {  
       $file = addslashes(file_get_contents($_FILES["image"]["tmp_name"]));  
-      $query = "INSERT INTO tbl_images(name) VALUES ('$file')";  
+      $query = "INSERT INTO pietanza(url_img) VALUES ('$file')";  
       if(mysqli_query($connect, $query))  
       {  
            echo '<script>alert("Image Inserted into Database")</script>';  
@@ -13,7 +20,7 @@
  <!DOCTYPE html>  
  <html>  
       <head>  
-           <title>Webslesson Tutorial | Insert and Display Images From Mysql Database in PHP</title>  
+           <title>Prova inserimento e visualizzazione immagine</title>  
            <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
@@ -21,12 +28,12 @@
       <body>  
            <br /><br />  
            <div class="container" style="width:500px;">  
-                <h3 style="align:center;">Insert and Display Images From Mysql Database in PHP</h3>  
+                <h3 style="align:center;">Prova inserimento immagini e visualizzazione</h3>  
                 <br />  
                 <form method="post" enctype="multipart/form-data">  
                      <input type="file" name="image" id="image" />  
                      <br />  
-                     <input type="submit" name="insert" id="insert" value="Insert" class="btn btn-info" />  
+                     <input type="submit" name="insert" id="insert" value="Inserisci" class="btn btn-info" />  
                 </form>  
                 <br />  
                 <br />  
@@ -35,14 +42,14 @@
                           <th>Image</th>  
                      </tr>  
                 <?php  
-                $query = "SELECT * FROM tbl_images ORDER BY id DESC";  
+                $query = "SELECT url_img FROM pietanza ORDER BY id";  
                 $result = mysqli_query($connect, $query);  
                 while($row = mysqli_fetch_array($result))  
                 {  
                      echo '  
                           <tr>  
                                <td>  
-                                    <img src="data:image/jpeg;base64,'.base64_encode($row['name'] ).'" height="200" width="200" class="img-thumnail" />  
+                                    <img src="data:image/jpeg;base64,'.base64_encode($row['url_img'] ).'" height="200" width="200" class="img-thumnail" />  
                                </td>  
                           </tr>  
                      ';  
@@ -64,7 +71,7 @@
            else  
            {  
                 var extension = $('#image').val().split('.').pop().toLowerCase();  
-                if(jQuery.inArray(extension, ['gif','png','jpg','jpeg']) == -1)  
+                if(jQuery.inArray(extension, ['png','jpg','jpeg']) == -1)  
                 {  
                      alert('Invalid Image File');  
                      $('#image').val('');  
