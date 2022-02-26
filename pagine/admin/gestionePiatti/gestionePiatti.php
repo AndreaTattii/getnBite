5dd7238cc587cb8f2405893df26f51add9bba2df
Giovanni <daniele.scaffai.2003@calvino.edu.it>
2022-02-26 10:40:20 +0100

    piccoli fix nella pagina gestione piatti

--- pagine/admin/gestionePiatti/gestionePiatti.php
+++ pagine/admin/gestionePiatti/gestionePiatti.php
@@ -15,7 +15,6 @@ session_start();
      <!-- Bootstrap CSS -->
      <link rel="stylesheet" href="../../../css/bootstrap.min.css">
 
-<<<<<<< HEAD
      <!-- font -->
      <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
 
@@ -77,6 +76,8 @@ session_start();
                     </div>
                </div>
           </div>
+          <br> 
+          <br>
           <div class="row"></div>
           <form class="row g-3">
                <div class="col-md-6">
@@ -87,6 +88,10 @@ session_start();
                     <label for="inputPassword4" class="form-label">Immagine del piatto</label>
                     <input type="password" class="form-control" id="inputPassword4" placeholder="url">
                </div>
+               <br>
+               <br>
+               <br>
+               <br>
                <div class="col-12">
                     <label for="inputAddress" class="form-label">Descrizione</label>
                     <textarea class="form-control" placeholder="Scrivi qui." name="recensione" id="recensione"></textarea>
@@ -134,111 +139,27 @@ session_start();
                          </div>
                     </div>
                </div>
+               <br>
+               <br>
+               <br>
+               <br>
+               <br>
+               <br>
+               <br>
+               <br>
                <div class="col-12">
-                    <button type="submit" class="btn btn-primary">Sign in</button>
+                    <button type="submit" class="btn btn-primary">Carica</button>
                </div>
           </form>
      </div>
      </div>
-
-
-
-     <br>
-     <div class="footer-clean">
-          <footer>
-               <div class="container">
-                    <div class="row justify-content-center">
-                         <div class="col-4 ">
-                              <a class="navbar-brand" href="./">
-                                   <img src="../../../img/home/logoScritta.png" alt="" height="60" class="d-inline-block align-text-top">
-                              </a>
-                         </div>
-                         <div class="col-4"></div>
-                         <center>
-                              <p style="text-decoration: none; color:black">Partita Iva: 02070920992</p>
-                              <p>GetnBite ©</p>
-                         </center>
-                    </div>
-               </div>
-     </div>
-     </footer>
-     </div>
-
-
-
-     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-     <script src="../../../js/bootstrap.min.js"></script>
-</body>
-
-</html>
-<?php
-
-?>
-=======
-    <title>GetnBite</title>
-    </head>
-    
-		  <body>
-
-		  
-			 <nav class="navbar  navbar-expand-lg navbar-light bg-light">
-				<div class="container p-2">
-				    <a class="navbar-brand" href="./">
-					   <img src="../../../img/Home/logoScritta.png" alt="" height="50" class="d-inline-block align-text-top">
-				    </a>
-				    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation" >
-					   <span class="navbar-toggler-icon"></span>
-				    </button>
-				    <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-					   <ul class="navbar-nav">
-						  <li class="nav-item">
-							 <a class="nav-link" href="../recensioni/recensioni.php" style="color: black">Recensioni</a>
-						  </li>
-						  <li class="nav-item">
-							 <a class="nav-link" href="../gestionePiatti/gestionePiatti.php" style="color: black">Gestione menu</a>
-						  </li>
-						  <li class="nav-item">
-							 <a class="nav-link" href="../consegne/consegne.php" style="color: black">Ordini</a>
-						  </li>
-						  <li class="nav-item">
-							 <a class="nav-link" href="../logout/logout.php" style="color: black">Logout</a>
-						  </li>
-						  
-						  
-					   </ul>
-				    </div>
-				</div>
-			 </nav>
-			 
-			 <div class="container">
-				<row class="row">
-				    <div class="col">
-					   <h1 style="font-family: 'Poppins'; text-align: center;">Il tuo ristorante</h1>
-				    </div>
-				</row>
-				<br>
-				<br>
-				<br>
-				<div class="row justify-content-center">
-				    <div class="col-4">
-					   <div style="border-top-color:#E4E4E4;  border-top-style: solid; border-top-width: 2px; border-bottom-color:#E4E4E4;  border-bottom-style: solid; border-bottom-width: 2px; text-align: center;">
-						  <container>
-							 <div class="row justify-content-center ">
-								<div class="col sm-6">
-								    <h2>Inserisci piatti<h2>
-								</div>
-							 </div>
-						  </container> 
-					   </div>
-				    </div>
-				</div>
-				<br>
-				<br>
-				<center>
-				<form action="../tabellaPiatti/tabellaPiatti.php">
-					<input type="submit" style="background-color: #00E1A5; color: white; display: inline-block; cursor: pointer; text-align: center; text-decoration: none; outline: none; border: none;  border-radius: 30px;" value="Visualizza i tuoi piatti">
-				</form>
-				</center>
+	<br>
+	<br>
+	<center>
+	<form action="../tabellaPiatti/tabellaPiatti.php">
+		<input type="submit" style="background-color: #00E1A5; color: white; display: inline-block; cursor: pointer; text-align: center; text-decoration: none; outline: none; border: none;  border-radius: 30px; height: 50px" value="Visualizza i tuoi piatti">
+	</form>
+	</center>
 				
 	   <br>
 	   <div class="footer-clean">
@@ -270,4 +191,3 @@ session_start();
 	   <?php
 		  
 	   ?>
->>>>>>> a81d6cad6f26a11cc4df2b7a3b35839172013e43
